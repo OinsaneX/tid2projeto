9c113bba5c947c6f034fd1a66a0b559ee31f04d5
OinsaneX <54595400+OinsaneX@users.noreply.github.com>
2020-09-28 13:29:15 -0700

    Updates

---
+++ precache-manifest.4af758ae810dea0c4bea931fb35ec4f0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2048f1f0ae246bd6787ec1fad80c72d",
+    "url": "/tid2projeto/index.html"
+  },
+  {
+    "revision": "5269eb1e4142d9257efa",
+    "url": "/tid2projeto/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "16d289513143d14ac3a9",
+    "url": "/tid2projeto/static/css/main.72ae64e0.chunk.css"
+  },
+  {
+    "revision": "5269eb1e4142d9257efa",
+    "url": "/tid2projeto/static/js/2.c8afeb3e.chunk.js"
+  },
+  {
+    "revision": "b6840d1fa4cd3c3e88b7c215b28b2e9e",
+    "url": "/tid2projeto/static/js/2.c8afeb3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16d289513143d14ac3a9",
+    "url": "/tid2projeto/static/js/main.cc250312.chunk.js"
+  },
+  {
+    "revision": "6f38365375f26094e4c5",
+    "url": "/tid2projeto/static/js/runtime-main.165df938.js"
+  },
+  {
+    "revision": "e6ec6ac8da59088f70b99f7b764fb729",
+    "url": "/tid2projeto/static/media/1.e6ec6ac8.jpg"
+  },
+  {
+    "revision": "fa42c5f5a6fc2a5b2f1100475d540585",
+    "url": "/tid2projeto/static/media/2.fa42c5f5.jpg"
+  },
+  {
+    "revision": "57c1ec7658e69ff19af90af93dd61f96",
+    "url": "/tid2projeto/static/media/3.57c1ec76.jpg"
+  },
+  {
+    "revision": "1703d75ad48a115919babb8649f01075",
+    "url": "/tid2projeto/static/media/login.1703d75a.jpeg"
+  }
+]);
